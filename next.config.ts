228e6171daedf0da035218fbb7765b3ce947bce2
Gabriel Rossanesi <gabrielrossanesi@outlook.com>
2026-04-17 17:45:35 -0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/brilhodaana",
+  assetPrefix: "/brilhodaana/",
   images: {
     remotePatterns: [
       {
